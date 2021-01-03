a3ae24f6cceadb04da571f28a0aa38492a0150de
Jamey Gronewald <jrgronewald@gmail.com>
2021-01-03 13:01:22 -0800

    backend for deleting an inventory item  working

--- controllers/itemController.ts
+++ controllers/itemController.ts
@@ -35,21 +35,43 @@ router.put('/:id', checkToken, async (req: any, res) => {
   const { id: userId } = req.user;
   
   try {
-    // const user = await db.User.findOne({ _id: userId }).populate('items');
-    // if (!user) throw new Error('Unable to create new item.');
-
     const itemToUpdate = await db.Item.findOne({ _id: id });
     let { status } = itemToUpdate;
 
     itemToUpdate.status = status === 'Inventory' ? 'Wishlist' : 'Inventory';
 
-    const item = await itemToUpdate.save();
+    await itemToUpdate.save();
 
-    // user.items.push(item);
-    // await user.save();
     const user = await db.User.findOne({ _id: userId }).populate('items');
+    if (!user) throw new Error('Unable to update item.');
 
     res.status(200).json({ user, message: 'Item was updated!' });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({
+      message: 'Server error.',
+    });
+  }
+});
+
+// DELETE AN ITEM
+router.delete('/:id', checkToken, async (req: any, res) => {
+  const { id } = req.params;
+  const { id: userId } = req.user;
+  
+  try {
+    const user = await db.User.findOne({ _id: userId }).populate('items');
+    if (!user) throw new Error('Unable to delete item.');
+  
+    const indexToRemove: number = user.items.map(item => item._id).indexOf(id);
+
+    user.items.splice(indexToRemove, 1);
+
+    await user.save();
+
+    await db.Item.findByIdAndDelete(id);
+
+    res.status(200).json({ user, message: 'Item was removed from inventory.' });
   } catch (error) {
     console.error(error.message);
     res.status(500).json({
