482706c0ce0fd47bafce0f99962d946c21062c76
Jamey Gronewald <jrgronewald@gmail.com>
2021-01-08 13:09:15 -0800

    removed localhost from user API requests

--- client/src/utils/API/userRequests.ts
+++ client/src/utils/API/userRequests.ts
@@ -6,10 +6,10 @@ const config = { headers: { 'Content-Type': 'application/json' } };
 
 export const userRequests = {
   register: (newUser: RegisterCredentials) =>
-    axios.post('http://localhost:3001/api/user/register', newUser, config),
+    axios.post('/api/user/register', newUser, config),
 
   loginUser: (credentials: LoginCredentials) =>
-    axios.post('http://localhost:3001/api/auth', credentials, config),
+    axios.post('/api/auth', credentials, config),
 
-  getUser: () => axios.get('http://localhost:3001/api/auth'),
+  getUser: () => axios.get('/api/auth'),
 };
